5fc3f38b5aa9207fc52f5dbcf6ffb1c61f78d353
[Alex] <alexandros.konstantakos@student.reutlingen-university.de>
2017-06-04 18:41:20 +0200

    Added file reader for the personal.json file

--- app/src/test/java/com/wua/mc/webuntisapp/ExampleUnitTest.java
+++ app/src/test/java/com/wua/mc/webuntisapp/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.wua.mc.webuntisapp;
 
+import com.wua.mc.webuntisapp.model.WebUntisClient;
+
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +13,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getCurrentSchoolYear() throws Exception{
+        JSONObject result = JSONFileReader.readPersonalData();
+
+        WebUntisClient wuc = new WebUntisClient(result.getString("username"), result.getString("password"), "HS+Reutlingen");
+        JSONObject res = new JSONObject("{\"result\":{\"id\": 7,\"name\": \"2016/2017\",\"startDate\": 20160829,\"endDate\": 20170731}}");
+        assertEquals(res.getJSONObject("result").toString(),
+                wuc.getCurrentSchoolYear().getJSONObject("response").getJSONObject("result").toString());
     }
 }
