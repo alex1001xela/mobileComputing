3b1ced5cb954ae86ec6349cbab3d2398040b4a0b
[Alex] <alexandros.konstantakos@student.reutlingen-university.de>
2017-06-05 19:41:29 +0200

    First commit for the personal calendar

--- app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
+++ app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
@@ -9,14 +9,14 @@ import android.os.Bundle;
 import android.os.IBinder;
 
 import com.wua.mc.webuntisapp.R;
+import com.wua.mc.webuntisapp.presenter.CalendarPresenter;
 
 public class MainActivity extends Activity {
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            Intent intent = new Intent("com.wua.mc.webuntisapp.view.GlobalCalendar");
-            startActivity(intent);
+
 
         }
 
@@ -31,8 +31,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent("com.wua.mc.webuntisapp.presenter.CalendarPresenter");
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        Intent intent1 = new Intent(this, CalendarPresenter.class);
+        bindService(intent1, connection, Context.BIND_AUTO_CREATE);
+        Intent intent2 = new Intent(this, GlobalCalendar.class);
+        startActivity(intent2);
     }
 
 
