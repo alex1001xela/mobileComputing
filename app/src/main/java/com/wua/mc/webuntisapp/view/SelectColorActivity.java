4a5e3bc78618d45169f69e27353a060cdb52f547
Bibi90 <Habiba.Boudjellali@Student.Reutlingen-University.DE>
2017-06-10 21:29:20 +0200

    Added SelectColorActivity Class
    for testing

---
+++ app/src/main/java/com/wua/mc/webuntisapp/view/SelectColorActivity.java
@@ -0,0 +1,41 @@
+package com.wua.mc.webuntisapp.view;
+
+/**
+ * Created by Manny on 10.06.2017.
+ */
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+        import com.wua.mc.webuntisapp.R;
+        import com.wua.mc.webuntisapp.presenter.CalendarPresenter;
+
+public class SelectColorActivity extends AppCompatActivity {
+
+    Button buttonSelectColor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent1 = new Intent(this, CalendarPresenter.class);
+        startService(intent1);
+//        Intent intent2 = new Intent(this, GlobalCalendar.class);
+//        startActivity(intent2);
+
+        buttonSelectColor = (Button) findViewById(R.id.buttonSelectColor);
+        buttonSelectColor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),SelectColorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
