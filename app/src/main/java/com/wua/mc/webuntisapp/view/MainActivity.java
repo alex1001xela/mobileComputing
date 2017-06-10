97a2a9a9ca5e7f57b81332d525c6d0cba3ded47a
Niko <nikobellic760@gmail.com>
2017-06-10 11:09:01 +0200

    Login finished

--- app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
+++ app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
@@ -15,12 +15,13 @@ import com.wua.mc.webuntisapp.R;
 import com.wua.mc.webuntisapp.model.WebUntisChecker;
 import com.wua.mc.webuntisapp.presenter.CalendarPresenter;
 
+import static com.wua.mc.webuntisapp.R.layout.activity_choose_fieldofstudy;
 import static com.wua.mc.webuntisapp.R.layout.activity_personal_calendar;
 
 public class MainActivity extends Activity {
 
     CalendarPresenter cp = new CalendarPresenter();
-
+    static boolean firstLogin=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,30 +31,37 @@ public class MainActivity extends Activity {
 
         startService(intent1);
 
+        if (firstLogin){
+            Button loginButton = (Button)this.findViewById(R.id.loginButton);
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
 
-        Button loginButton = (Button)this.findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+                    try {
+                        EditText username   = (EditText)findViewById(R.id.usernameField);
+                        EditText password   = (EditText)findViewById(R.id.passwordField);
 
-                try {
-                    EditText username   = (EditText)findViewById(R.id.usernameField);
-                    EditText password   = (EditText)findViewById(R.id.passwordField);
+                        String u= username.getText().toString();
+                        String p = password.getText().toString();
+                        Log.v(u,p );
 
-                    String u= username.getText().toString();
-                    String p = password.getText().toString();
-                    Log.v(u,p );
+                        cp.login(username.getText().toString(), password.getText().toString());
+                        Log.v("statusLogin","Login Successfull");
+                        setContentView(activity_choose_fieldofstudy);
+                    }catch (Exception e){
+                        Log.v("statusLogin","Login Failed");
+                        Toast errorToast = Toast.makeText(getApplication() , "Login data wrong", Toast.LENGTH_SHORT);
+                        errorToast.show();
 
-                    cp.login(username.getText().toString(), password.getText().toString());
-                    Log.v("statusLogin","Login Successfull");
-                    setContentView(activity_personal_calendar);
-                }catch (Exception e){
-                    Log.v("statusLogin","Login Failed");
-                    Toast errorToast = Toast.makeText(getApplication() , "Login data wrong", Toast.LENGTH_SHORT);
-                    errorToast.show();
-
+                    }
                 }
-            }
-        });
+            });
+
+        }else{
+            setContentView(activity_personal_calendar);
+        }
+
+
+
 
     }
 
