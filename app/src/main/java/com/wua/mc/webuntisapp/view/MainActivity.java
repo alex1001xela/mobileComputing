2dc5f67ea7125ae250b33e6a9427b484314e9673
Bibi90 <Habiba.Boudjellali@Student.Reutlingen-University.DE>
2017-06-12 00:52:16 +0200

    buttonSelectColor auskommentiert
    wird noch entsprechend Mockup angepasst

--- app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
+++ app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
                         Log.v("statusLogin","Login Successfull");
                         setContentView(activity_choose_fieldofstudy);
+                        /*
 						Button buttonSelectColor = (Button) findViewById(R.id.buttonSelectColor);
                         buttonSelectColor.setOnClickListener(new View.OnClickListener(){
                             @Override
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                                 event = (TextView) findViewById(R.id.textCourse);
                                 event.setBackgroundResource(R.color.white);
                             }
-                        });
+                        });*/
                         firstLogin=false;
                     }catch (Exception e){
                         Log.v("statusLogin","Login Failed");
