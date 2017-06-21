c83952a098c14519408d0965d018101368a67f73
tanifort <Ramond_Belmo.Tanifor_tamah@Student.Reutlingen-University.DE>
2017-06-21 18:16:08 +0200

    Merge branch 'master' of https://github.com/alexandrosntak/mobileComputing
    # Conflicts:
    #	app/src/main/java/com/wua/mc/webuntisapp/view/MainActivity.java

--- app/src/test/java/com/wua/mc/webuntisapp/WebUntisAPI.java
+++ app/src/test/java/com/wua/mc/webuntisapp/WebUntisAPI.java
@@ -3,12 +3,9 @@ package com.wua.mc.webuntisapp;
 import com.wua.mc.webuntisapp.model.WebUntisClient;
 import com.wua.mc.webuntisapp.model.iWebUntisClient;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -23,15 +20,12 @@ public class WebUntisAPI {
 
 
         //String jsonSample = .toString();
-        JSONObject jsonObject = wuc.getClasses();
+        JSONObject jsonObject = wuc.getCurrentSchoolYear();
+       // JSONObject jsonObject = wuc.getClasses();
         //JSONArray jsonObject = wuc.getFilters();
         System.out.print(jsonObject.toString());
         try {
 
-            JSONArray result = jsonObject.getJSONObject("response").getJSONArray("result");
-           // JSONObject result = jsonObject.getJSONObject("response").getJSONObject("result");
-            System.out.print("------------------------------------------------>");
-            //System.out.print(result.getJSONObject(0).get("longName"));
 
             /**
              by -ray :
@@ -40,35 +34,40 @@ public class WebUntisAPI {
              afterwads the result is looped (this first for loop) on in other retrieve the attribute = longName and stored dynamically in another Array list ( COS) .
              the second for loop  is to verify if the outputs are the ones expected. the || tokens are used to separate the COS.
              .
+             JSONArray result = jsonObject.getJSONObject("response").getJSONArray("result");
+             // JSONObject result = jsonObject.getJSONObject("response").getJSONObject("result");
+             System.out.print("------------------------------------------------>");
+             //System.out.print(result.getJSONObject(0).get("longName"));
 
+             ArrayList<String> All_course_of_study = new ArrayList<>();
+             for(int i=0;i< result.length();i++){
+             String name = result.getJSONObject(i).getString("longName"); // retrieving the Long name of each course of study as string
+
+             // if(!All_course_of_study.contains(name)){
+             All_course_of_study.add(name);
+             //  }
+             // storing the course of study in a a list (ALL_course_of_study)
+
+             }
+
+             for ( int i = 0; i< All_course_of_study.size();i++){
+             // System.out.print(All_course_of_study.get(i));
+             // System.out.print("||");
+             }
 
 
              */
 
-            ArrayList<String> All_course_of_study = new ArrayList<>();
-            for(int i=0;i< result.length();i++){
-                String name = result.getJSONObject(i).getString("longName"); // retrieving the Long name of each course of study as string
-
-               // if(!All_course_of_study.contains(name)){
-                    All_course_of_study.add(name);
-              //  }
-                // storing the course of study in a a list (ALL_course_of_study)
-
-            }
-
-            for ( int i = 0; i< All_course_of_study.size();i++){
-               // System.out.print(All_course_of_study.get(i));
-               // System.out.print("||");
-            }
 
 
 
-           // JSONObject result = jsonObject.getJSONObject("response").getJSONObject("result");
-            /*result.getInt("id");
+
+            JSONObject result = jsonObject.getJSONObject("response").getJSONObject("result");
+            result.getInt("id");
             result.getString("name");
             result.getInt("startDate");
             result.getInt("endDate");
-            */
+
         }
         catch (JSONException e){
             throwedException = true;
@@ -84,6 +83,13 @@ public class WebUntisAPI {
         }
 
         assertTrue(throwedException);
+    }
+    @Test
+    public void getClasses() throws Exception{
+        boolean throwedException = false;
+        JSONObject jsonObject = wuc.getClasses();
+        System.out.print(jsonObject.toString());
+
     }
 
     //Test für ADD mit (query)select kann man werte abfragen
